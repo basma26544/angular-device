1d5aa34b57eb32d529003f3663bbe704a2796047
basma26544 <113221934+basma26544@users.noreply.github.com>
2022-09-28 09:03:07 -0400

    4 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-
+import { AppBootstrapModule } from './app-bootstrap/app-bootstrap.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -11,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppBootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
